a5cab202e2aac21888b5c26b4be8a321a17be8a6
ae2f <mincraft20046@gmail.com>
2025-06-21 04:13:39 +0900

    Macro.cmake

--- cmake/Macro.cmake
+++ cmake/Macro.cmake
@@ -4,14 +4,33 @@ function(ae2f_Macro_init prm_CMT_REQUIRED prm_SZPARAM prm_SZTPARAM)
 	file(REMOVE ${ae2f_Macro_ROOT}/build/Macro)
   	message("[ae2f_Macro_init]  ${CMAKE_GENERATOR}")
 
+	if(${CMAKE_C_STANDARD})
+		set(cstd "-DCMAKE_C_STANDARD=${CMAKE_C_STANDARD}")
+	else()
+		set(cstd "")
+	endif()
+
+	if(${CMAKE_C_COMPILER})
+		set(cc "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
+	else()
+		set(cc "")
+	endif()
+
+	if(${CMAKE_GENERATOR})
+		set(gen "-G${CMAKE_GENERATOR}")
+	else()
+		set(gen "")
+	endif()
+
 	execute_process(
 		WORKING_DIRECTORY ${ae2f_Macro_ROOT}
 		COMMAND ${CMAKE_COMMAND} 
-    "-S" "." "-B" "./build" "-G" "${CMAKE_GENERATOR}" 
+		"-S" "." "-B" "./build"
 		-Dae2f_Macro_CMT_REQUIRED=${prm_CMT_REQUIRED}
 		-Dae2f_Macro_SZPARAM=${prm_SZPARAM}
 		-Dae2f_Macro_SZTPARAM=${prm_SZTPARAM}
-        -Dae2f_MAC_KEYWORD=${ae2f_MAC_KEYWORD}
+        	-Dae2f_MAC_KEYWORD=${ae2f_MAC_KEYWORD}
+		${gen} ${cstd} ${cc}
 		${ARGN}
   		RESULT_VARIABLE ConfOut
 	)
